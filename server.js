b532d00d4d1ae7c3b5f553ebfbf2a1ce7b6ad971
MuhammedFayistgl <77223454+MuhammedFayistgl@users.noreply.github.com>
2023-09-07 15:42:29 +0530

    chanch cors credentials: true

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(function (req, res, next) {
   if (allowedDomains.indexOf(origin) > -1) {
     res.setHeader('Access-Control-Allow-Origin', origin);
   }
-
+app.use(cors({credentials: true}))
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Accept');
   res.setHeader('Access-Control-Allow-Credentials', true);
