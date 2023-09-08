34672858e0e1b97a3d06b9e5d66e0a3cbdee169b
MuhammedFayistgl <77223454+MuhammedFayistgl@users.noreply.github.com>
2023-09-08 14:12:45 +0530

    Updates

--- assets/AxiosINSTENCE-73d1f499.js
+++
@@ -1,1 +0,0 @@
-import{o}from"./index-fbe38c32.js";const e=o.create({baseURL:"https://doctorappointment-64iw.onrender.com/"});export{e as A};
--- assets/PublicRoute-723236ee.js
+++
@@ -1,1 +0,0 @@
-import{j as t,N as n}from"./index-fbe38c32.js";import{u as r}from"./useCookies-98b20210.js";function l(o){const[e,s,i]=r(["token"]);return console.log(e==null?void 0:e.token,"cookies?.token"),e!=null&&e.token?t.jsx(n,{to:"/"}):o.children}export{l as default};
