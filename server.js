c995556bd23b7e7ef0116a26db12839bc54bd855
MuhammedFayistgl <77223454+MuhammedFayistgl@users.noreply.github.com>
2023-09-08 09:09:42 +0530

    Change jwt token expdate change

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const path = require("path");
 const  cookieParser = require('cookie-parser')
 
 var corsoption={
-  origin:"https://muhammedfayistgl.github.io", //origin from where you requesting // "http://localhost:3000",
+  origin:["https://muhammedfayistgl.github.io","http://localhost:3000"], //origin from where you requesting // "http://localhost:3000",
   credentials:true
 }
 
