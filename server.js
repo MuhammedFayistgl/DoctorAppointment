f9480614281a33a45520a5d8d39bdf06b41f0fcc
MuhammedFayistgl <77223454+MuhammedFayistgl@users.noreply.github.com>
2023-09-10 17:34:16 +0530

    Chaenge dir err fix

--- server.js
+++ server.js
@@ -36,13 +36,13 @@ app.use(function (req, res, next) {
   next();
 });
 
-if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static("client/build"));
+// if (process.env.NODE_ENV === "production") {
+//   app.use("/", express.static("client/build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client/dist/index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client/dist/index.html"));
+//   });
+// }
 const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => res.send("Hello World!"));
