cac76bc6274db6b56d6ebacb9d0dda912164c5e7
MuhammedFayistgl <77223454+MuhammedFayistgl@users.noreply.github.com>
2023-09-11 16:12:24 +0530

    client file deleted

--- client/src/components/ProfilAvathar.jsx
+++
@@ -1,7 +0,0 @@
-import { Avatar } from "rsuite";
-
-const ProfilAvathar = () => {
-	return <Avatar size="lg" circle src="https://avatars.githubusercontent.com/u/12592949" alt="@SevenOutman" />;
-};
-
-export default ProfilAvathar;
