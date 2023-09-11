3f255cd3cd35b3966b8a0d3fdc30e02c31ea7190
MuhammedFayistgl <77223454+MuhammedFayistgl@users.noreply.github.com>
2023-09-11 13:46:05 +0530

    cookie err  httponly false

--- client/src/utils/AxiosINSTENCE.js
+++ client/src/utils/AxiosINSTENCE.js
@@ -5,11 +5,11 @@ export const AxiosConnection = axios.create({
   // baseURL: 'https://cute-plum-angler-cuff.cyclic.app',
   // Producton url 
   // baseURL: 'http://localhost:5000/',
-  // mode: "cors",
+  mode: "cors",
   withCredentials: true, // should be there
   credentials: 'include', // should be there
-  headers: {
-    Authorization: "Bearer " + document.cookie,
-  }
+  // headers: {
+  //   Authorization: "Bearer " + document.cookie,
+  // }
 }
 )
