d188abed77241915b0ae6405c6ba205b9dd69d35
MuhammedFayistgl <77223454+MuhammedFayistgl@users.noreply.github.com>
2023-09-14 22:03:33 +0530

    deploy succussfully

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -31,13 +31,13 @@ function ProtectedRoute(props) {
 			if (response.data.success) {
 				dispatch(setUser(response.data.data));
 			} else {
-				removeCookie("token");
+				// removeCookie("token");
 				navigate("/login");
 			}
 		} catch (error) {
 			
 			dispatch(hideLoading());
-			removeCookie("token");
+			// removeCookie("token");
 			navigate("/login");
 		}
 	};
