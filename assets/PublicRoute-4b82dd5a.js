b039d6d6683a62d5755e647d348abd7f11289540
MuhammedFayistgl <77223454+MuhammedFayistgl@users.noreply.github.com>
2023-09-18 17:21:10 +0530

    Updates

---
+++ assets/PublicRoute-4b82dd5a.js
@@ -0,0 +1,1 @@
+import{j as t,N as n}from"./index-645bad21.js";import{u as r}from"./useCookies-01585a5f.js";function l(o){const[e,s,i]=r(["token"]);return console.log(e==null?void 0:e.token,"cookies?.token"),e!=null&&e.token?t.jsx(n,{to:"/"}):o.children}export{l as default};
